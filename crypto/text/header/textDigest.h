76fec874436d93585465280748de2011df85d2e7
Romain GOUPIL <romain.goupil@netc.fr>
2021-05-11 07:15:52 +0200

    Created text digest functions.

---
+++ crypto/text/header/textDigest.h
@@ -0,0 +1,28 @@
+//
+// Created by goupilr on 11/05/2021.
+//
+
+#ifndef CRYPTTOOL_TEXTDIGEST_H
+#define CRYPTTOOL_TEXTDIGEST_H
+
+#include "openssl/evp.h"
+
+/**
+ * Return the SHA256 sum of text.
+ *
+ * @param text Text to be hashed.
+ * @param textSize Size of text.
+ * @return The SHA256 hash of text.
+ */
+unsigned char* textDigestSHA256(unsigned char* text, unsigned int textSize);
+
+/**
+ * Return the MD5 sum of text.
+ *
+ * @param text Text to be hashed.
+ * @param textSize Size of text.
+ * @return The MD5 hash of text.
+ */
+unsigned char* textDigestMD5(unsigned char* text, unsigned int textSize);
+
+#endif //CRYPTTOOL_TEXTDIGEST_H
---
+++ crypto/text/textDigest.c
@@ -0,0 +1,56 @@
+//
+// Created by goupilr on 11/05/2021.
+//
+
+#include "header/textDigest.h"
+#include "../error.h"
+
+unsigned char* textDigestSHA256(unsigned char* text, unsigned int size)
+{
+    EVP_MD_CTX* ctx = NULL;
+    unsigned char* hash = NULL;
+    unsigned int* tempSize = NULL;
+
+    if (!(ctx = EVP_MD_CTX_new()))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestInit_ex(ctx, EVP_sha256(), NULL))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestUpdate(ctx, text, size))
+        handleSSLErrors();
+
+    if (!(hash = (unsigned char*) OPENSSL_malloc(EVP_MD_size(EVP_sha256()))))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestFinal_ex(ctx, hash, tempSize))
+        handleSSLErrors();
+
+    free(tempSize);
+    return hash;
+}
+
+unsigned char* textDigestMD5(unsigned char* text, unsigned int size)
+{
+    EVP_MD_CTX* ctx = NULL;
+    unsigned char* hash = NULL;
+    unsigned int* tempSize = NULL;
+
+    if (!(ctx = EVP_MD_CTX_new()))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestInit_ex(ctx, EVP_md5(), NULL))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestUpdate(ctx, text, size))
+        handleSSLErrors();
+
+    if (!(hash = (unsigned char*) OPENSSL_malloc(EVP_MD_size(EVP_md5()))))
+        handleSSLErrors();
+
+    if (1 != EVP_DigestFinal_ex(ctx, hash, tempSize))
+        handleSSLErrors();
+
+    free(tempSize);
+    return hash;
+}
