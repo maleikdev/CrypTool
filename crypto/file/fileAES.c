3457c3020c1fd08a20a76aecd0af683088f2abb3
Romain GOUPIL <romain.goupil@netc.fr>
2021-05-28 06:45:17 +0200

    Implementing a hash translator.

--- crypto/file/fileAES.c
+++ crypto/file/fileAES.c
@@ -3,13 +3,22 @@
 //
 
 #include "header/fileAES.h"
+#include "../text/header/textAES.h"
 
 void encryptFileInfoAES256(FileInfo* file, char* key)
 {
+    unsigned char* iv = encryptTextAES256(file->m_fileContent, &file->m_fileSize, key);
 
+    unsigned char* tempFileContent = (unsigned char*) calloc (16 + file->m_fileSize, sizeof (unsigned char));
+    strcpy(tempFileContent, iv);
+    strcpy(tempFileContent + 16, file->m_fileContent);
+    file->m_fileSize += 16;
 }
 
 void decryptFileInfoAES256(FileInfo* file, char* key)
 {
+    unsigned char* iv = NULL;
+    memcpy(iv, file->m_fileContent, 16);
 
+    decryptTextAES256(file->m_fileContent + 16, )
 }
