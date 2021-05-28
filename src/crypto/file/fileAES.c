5ab2686ce5fbfa9f101143739e7045b0eff7e843
Romain GOUPIL <romain.goupil@netc.fr>
2021-05-28 06:54:01 +0200

    Moving files to a new directory.

--- src/crypto/file/fileAES.c
+++ src/crypto/file/fileAES.c
@@ -5,7 +5,7 @@
 #include "header/fileAES.h"
 #include "../text/header/textAES.h"
 
-void encryptFileInfoAES256(FileInfo* file, char* key)
+void encryptFileInfoAES256(FileInfo* file, unsigned char *key)
 {
     unsigned char* iv = encryptTextAES256(file->m_fileContent, &file->m_fileSize, key);
 
@@ -15,10 +15,10 @@ void encryptFileInfoAES256(FileInfo* file, char* key)
     file->m_fileSize += 16;
 }
 
-void decryptFileInfoAES256(FileInfo* file, char* key)
+void decryptFileInfoAES256(FileInfo* file, unsigned char *key)
 {
     unsigned char* iv = NULL;
     memcpy(iv, file->m_fileContent, 16);
-
-    decryptTextAES256(file->m_fileContent + 16, )
+    file->m_fileSize -= 16;
+    decryptTextAES256(file->m_fileContent + 16, &file->m_fileSize, key, iv);
 }
